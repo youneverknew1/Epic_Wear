6d3d47429cc2549acfd462af705c0f6a11b234df
youneverknew1 <sadidcse55@gmail.com>
2026-07-17 13:49:30 +0600

    api fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,8 @@ const API = axios.create({
   baseURL: 'https://epic-wear-backend.onrender.com/api'
 });
 
-export const getProducts = () => API.get('/products');
+export const getProducts = (edition) => 
+  API.get('/products', { params: edition ? { edition } : {} });
 export const getProduct = (id) => API.get(`/products/${id}`);
 export const getCategories = () => API.get('/categories');
 export const getCart = (userId) => API.get(`/cart/${userId}`);
